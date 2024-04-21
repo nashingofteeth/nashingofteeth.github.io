5331368c4abe7d9a2559a26cfcdefc998cf16b77
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2024-04-21 13:57:08 -0700

    fix images

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,7 +6,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
 
-	eleventyConfig.addPassthroughCopy("public");
+	eleventyConfig.addPassthroughCopy( { 'public' : '/' } );
 	eleventyConfig.addPassthroughCopy("tools");
 	eleventyConfig.addPassthroughCopy(".htaccess");
 	eleventyConfig.addPassthroughCopy("CNAME");
