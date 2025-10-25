c6519364b213c4ee415e1f8dbac04f6245e6d33e
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2025-10-25 15:14:12 -0700

    move css files to public folder

--- templates/partials/css-loader.js
+++ templates/partials/css-loader.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const path = require("path");
 
 /**
- * Load and combine multiple CSS files
+ * Load and combine multiple CSS files from public/css/
  * @param {...string} cssFileNames - CSS file names (without path)
  * @returns {string} Combined CSS content
  */
 function loadCss(...cssFileNames) {
   const cssContents = cssFileNames.map((fileName) => {
-    const filePath = path.join(__dirname, "..", fileName);
+    const filePath = path.join(__dirname, "..", "..", "public", "css", fileName);
     return fs.readFileSync(filePath, "utf8");
   });
 
