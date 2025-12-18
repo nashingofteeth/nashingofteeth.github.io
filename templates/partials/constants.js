10928bd204ff5bae23ff0ec3cd1fc93bcfa88870
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2025-12-17 19:40:09 -0800

    update video masters path

--- templates/partials/constants.js
+++ templates/partials/constants.js
@@ -4,7 +4,7 @@ const STORJ_BASE_URL =
   "https://link.storjshare.io/raw/jx2jhmkhn6jlw53upq2dw2t5jomq/nash-video/";
 
 const STORJ_LOSSLESS_URL =
-  "https://link.storjshare.io/raw/jvl6tf34ep5hgpycmohsmset4cwq/backups/Projects%2FFilmography%2FMasters/";
+  "https://link.storjshare.io/raw/jvl6tf34ep5hgpycmohsmset4cwq/backups/archive/filmography/masters/";
 
 module.exports = {
   STORJ_BASE_URL,
